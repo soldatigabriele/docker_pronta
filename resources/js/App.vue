08806312d4d44eb6e2a791430e978539566d01cf
Gab <gabriele.soldati@globusgroup.com>
2025-05-23 14:33:02 +0100

    Try make vue work

---
+++ resources/js/App.vue
@@ -0,0 +1,6 @@
+<template>
+  <h1>Hello from Vue!</h1>
+</template>
+
+<script setup>
+</script>
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,16 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-        tailwindcss(),
-    ],
-});
+    plugins: [laravel(['resources/js/app.js']), vue()],
+    server: {
+      host: '0.0.0.0',
+      port: 5173,
+      strictPort: true,
+      hmr: {
+        host: 'localhost',
+      },
+    },
+  });
+  
